e02927c7348dae5cacadf4292dcb6c708eea02d9
Kaylee (Kay) <77746492+kayillustrations@users.noreply.github.com>
2025-07-03 11:29:01 -0400

    more shaders

--- scenes/camera_2d.gd
+++ scenes/camera_2d.gd
@@ -1,8 +1,10 @@
 extends Camera2D
 
+@onready var canvas_layer: CanvasLayer = $"../CanvasLayer"
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	
+	canvas_layer.visible = true
 	pass # Replace with function body.
 
 
