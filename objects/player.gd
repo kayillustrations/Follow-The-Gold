ae8b099c5be4179d542d32e62d80228ce9cc7af7
Kaylee (Kay) <77746492+kayillustrations@users.noreply.github.com>
2025-07-02 21:30:39 -0400

    backward Movement

--- objects/player.gd
+++ objects/player.gd
@@ -3,11 +3,12 @@ extends CharacterBody2D
 @onready var health_comp: HealthComp = $HealthComp
 @onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var screen_dimensions = Vector2(get_viewport().size)
+var player_position_uv : Vector2
 
-const SPEED = 300.0
+const SPEED = 500.0
 const JUMP_VELOCITY = -400.0
 
-var player_position_uv : Vector2
+var b_movement:float = -150
 
 func _ready() -> void:
 	GameManager.player = self
@@ -16,7 +17,9 @@ func _process(delta) -> void:
 	if SignalBus.isBlinded: Blindness()
 
 func _physics_process(delta: float) -> void:
+	Movement()
 
+func Movement():
 	## Handle jump.
 	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		#velocity.y = JUMP_VELOCITY
@@ -32,6 +35,8 @@ func _physics_process(delta: float) -> void:
 		velocity = Vector2(direction_x,direction_y) * SPEED/1.5
 	else:
 		velocity = Vector2(direction_x,direction_y) * SPEED
+
+	velocity.x += b_movement
 
 	move_and_slide()
 
