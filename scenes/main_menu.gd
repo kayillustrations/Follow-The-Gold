efba2ce87d39910c0cd0d8886cf55966504283e5
Kaylee (Kay) <77746492+kayillustrations@users.noreply.github.com>
2025-07-30 17:10:49 -0400

    tried focus

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -1,6 +1,10 @@
 extends Control
 
+var starts_focus: Button
+var is_focused: Button
+
 func _ready() -> void:
+	starts_focus = $Panel/Start
 	if SceneLoader.ui.visible:
 		SceneLoader.UISceneActivate(SceneLoader.ui)
 		SceneLoader.ui.timer.stop()
@@ -8,6 +12,25 @@ func _ready() -> void:
 	SceneLoader.ConnectButtons(self)
 	SceneLoader.DeleteAllTemp()
 	MusicManager._switchmusic("res://sound/music/titlescreenloop_v1.ogg", "res://sound/music/titlescreenloop_v1_wrappedloop.ogg", -4)
+
+func _process(delta: float) -> void:
+	pass
+	#if Input.is_anything_pressed() && is_focused == null && !SceneLoader.inTempScene:
+		#starts_focus.grab_focus()
+		#is_focused = starts_focus
+		#return
+	#if Input.is_action_just_pressed("right"):
+		#is_focused.focus_neighbor_right
+	#if Input.is_action_just_pressed("left"):
+		#is_focused.focus_neighbor_left
+	#if Input.is_action_just_pressed("up"):
+		#is_focused.focus_neighbor_top
+	#if Input.is_action_just_pressed("down"):
+		#is_focused.focus_neighbor_bottom
+	#if Input.is_action_just_pressed("ui_accept"):
+		#is_focused.release_focus()
+		#is_focused = null
+	#if is_focused: print(is_focused.name)
 
 func _on_start_pressed() -> void:
 	SceneLoader.inMenu = false
